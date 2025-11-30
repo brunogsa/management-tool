395db01546bc90137fa9198071ca54a53065698c
Bruno Salomão <brunoagostini@arcoeducacao.com.br>
2025-11-30 16:22:29 -0300

    feat(cli): show help after errors for commands
    - Add .showHelpAfterError() to root program
    - Add .showHelpAfterError() to tasks-tree command
    - Add .showHelpAfterError() to monte-carlo command

--- src/index.js
+++ src/index.js
@@ -4,17 +4,20 @@ import monteCarlo from './commands/monte-carlo.js';
 
 program
   .version('1.0.0')
-  .description('Collections of utilities for managing engineering projects and teams.');
+  .description('Collections of utilities for managing engineering projects and teams.')
+  .showHelpAfterError();
 
 program
   .command('tasks-tree <input-json-filepath> <output-folder-filepath>')
   .description('Generate a Mermaid flowchart visualizing task dependencies as a DAG')
   .option('-w, --watch', 'Watch input file and source code for changes and auto-regenerate with live browser preview')
+  .showHelpAfterError()
   .action(tasksTree);
 
 program
   .command('monte-carlo <json-input-filepath> <output-folder-filepath>')
   .description('Run Monte Carlo simulation to predict project completion time distribution')
+  .showHelpAfterError()
   .action(monteCarlo);
 
 program.parse(process.argv);
